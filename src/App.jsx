985447488bf3ac42b5d17bcdcd19599845e31434
vthani25 <vthanishkka@gmail.com>
2025-07-15 12:07:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Homepage } from './pages/Homepage'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,8 @@
+export const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the main page of our application.</p>
+    </div>
+  );
+}
